ca078a801427b2f2915899d2a56f295a67f49e27
DelleyAL <alrick.delley@heig-vd.ch>
2024-10-02 07:58:46 +0000

    code de la loi d'ohm

--- TD20240920/td.c
+++ TD20240920/td.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include <stdio.h> // inclusion du fichier entete (header) stdio.h
 
-int main(int argc, const char *argv[])
-{
-	puts("Saluto pedro!");
+int main(int argc, const char *argv[])// point d'entrée du programme
+{// debut du bloc main
+	puts("Saluto pedro!");//instructrion de d'affichage
 
-	return 0;
-}
+	return -1; // instruction de fin de programme 
+	
+}//fin du blocc main
